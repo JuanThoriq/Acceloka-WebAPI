3553dd062aa4ab5a343806babbf71c6115b2c166
JuanThoriq <juanthoriq@gmail.com>
2025-03-07 16:50:03 +0700

    add gitkeep

--- Acceloka/obj/Debug/net8.0/Acceloka.AssemblyInfo.cs
+++ Acceloka/obj/Debug/net8.0/Acceloka.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Acceloka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e4a4d67df02f87760bdd0215a718d13263a4ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1addb657c77bd0438e58a12dd9cac095571e193d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Acceloka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Acceloka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
